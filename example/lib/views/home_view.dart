01a8626275bd75081d45bcfb2d7189df7f93860f
Brian Manuputty <brian.manuputty@wolfpackit.nl>
2022-09-26 07:49:30 +0200

    [0.0.1+3] Add ListInformer and MapInformer shells, widget clean up

--- example/lib/views/home_view.dart
+++ example/lib/views/home_view.dart
@@ -1,22 +1,43 @@
+import 'package:example/widgets/informer_example.dart';
+import 'package:example/widgets/map_informer_example.dart';
 import 'package:flutter/material.dart';
 import 'package:veto/base_view_model.dart';
 
+import '../widgets/list_informer_example.dart';
 import 'home_view_model.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
   static const String route = 'home-view';
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>(
+      rebuild: false,
       builder: (context, model) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Informers Demo'),
-          ),
-          body: const Center(
-            child: Text('Test'),
+        return GestureDetector(
+          onTap: model.focusNode.unfocus,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Informers Example Project'),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(height: 24),
+                  InformerExample(model: model),
+                  const SizedBox(height: 24),
+                  ListInformerExample(model: model),
+                  const SizedBox(height: 24),
+                  MapInformerExample(model: model),
+                ],
+              ),
+            ),
           ),
         );
       },
--- example/lib/views/home_view_model.dart
+++ example/lib/views/home_view_model.dart
@@ -1,34 +1,27 @@
 import 'package:flutter/foundation.dart';
-import 'package:informers/informer.dart';
-import 'package:informers/list_informer.dart';
-import 'package:informers/map_informer.dart';
+import 'package:informers/informers.dart';
 import 'package:veto/base_view_model.dart';
 
 class HomeViewModel extends BaseViewModel {
-  
-  final Informer<int> _count = Informer(0);
-  ValueListenable<int> get count => _count;
-
-  final ListInformer<String> _names = ListInformer([]);
-  ValueListenable<List<String>> get names => _names;
-
-  final MapInformer<String, bool> _stringBool = MapInformer({});
-  ValueListenable<Map<String, bool>> get stringBool => _stringBool;
-
-  void increment() => _count.updateCurrent((value) => value + 1);
-
-  void reset() => _count.update(0);
-
+  final Informer<int> _counter = Informer(0);
+  ValueListenable<int> get counterListenable => _counter;
 
   @override
-  Future<void> initialise({arguments}) async {
+  Future<void> initialise() async {
     super.initialise();
   }
 
   @override
   void dispose() {
+    _counter.dispose();
     super.dispose();
   }
+
+  void updateInformer({required int value}) => _counter.update(value);
+
+  void decrementCounter() => _counter.updateCurrent((value) => --value);
+
+  void incrementCounter() => _counter.updateCurrent((value) => ++value);
 
   static HomeViewModel get locate => HomeViewModel();
 }
